44b09522e8657409f65b9cab63287800b60a4d08
Arjuna Panji <arjuna.panji97@gmail.com>
2022-08-23 02:29:09 +0700

    hide import anggota

--- application/controllers/anggota.php
+++ application/controllers/anggota.php
@@ -9,7 +9,8 @@ class Anggota extends OperatorController {
 	public function index() {
 		$this->data['judul_browser'] = 'Data';
 		$this->data['judul_utama'] = 'Data';
-		$this->data['judul_sub'] = 'Anggota <a href="'.site_url('anggota/import').'" class="btn btn-sm btn-success">Import Data</a>';
+		// $this->data['judul_sub'] = 'Anggota <a href="'.site_url('anggota/import').'" class="btn btn-sm btn-success">Import Data</a>';
+		$this->data['judul_sub'] = 'Anggota';
 
 		$this->output->set_template('gc');
 
@@ -122,6 +123,7 @@ class Anggota extends OperatorController {
 		$out['level'] = $this->data['level'];
 
 		$this->load->view('default_v', $output);
+
 	}
 
 
--- application/views/default_v.php
+++ application/views/default_v.php
@@ -1,15 +1,1 @@
-<?php 
-echo $output; 
-?>
-
-<script>
-    $(document).ready(function(){
-    $('#field-tgl_lahir').datepicker(
-        {
-            changeMonth: false,
-            changeYear: false,
-            yearRange: '1950:2011'
-        }
-    );
-});
-</script>
+<?php echo $output; ?>

cbb891f4b77b6b67f17abd5f7bbc05c56323f541
Arjuna Panji <arjuna.panji97@gmail.com>
2022-08-23 21:24:36 +0700

    add import

--- application/controllers/anggota.php
+++ application/controllers/anggota.php
@@ -10,7 +10,7 @@ class Anggota extends OperatorController {
 		$this->data['judul_browser'] = 'Data';
 		$this->data['judul_utama'] = 'Data';
 		$this->data['judul_sub'] = 'Anggota <a href="'.site_url('anggota/import').'" class="btn btn-sm btn-success">Import Data</a>';
-		$this->data['judul_sub'] = 'Anggota';
+		// $this->data['judul_sub'] = 'Anggota';
 
 		$this->output->set_template('gc');
 
